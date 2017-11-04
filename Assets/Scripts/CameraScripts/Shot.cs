1307e5a247c700f209e30b9f71e74bf77155b5b2
Wesley King <kingwe92@gmail.com>
2017-11-04 13:40:33 -0400

    UpdateCamera Scripts to utilize Shots/ShotZones

---
+++ Assets/Scripts/CameraScripts/Shot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Shot : MonoBehaviour
+{
+    public Vector3 focalPoint;
+    public void CutToShot()
+    {
+        transform.LookAt(focalPoint);
+        Camera.main.transform.localPosition = transform.position;
+        Camera.main.transform.localRotation = transform.rotation;
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (!Application.isPlaying)
+        {
+            CutToShot();
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, focalPoint);
+    }
+}
